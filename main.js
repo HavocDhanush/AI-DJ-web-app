be9b67b23c4571b5ceaf56c694eb5aef8ef37e64
HavocDhanush <93704624+HavocDhanush@users.noreply.github.com>
2023-12-11 19:07:44 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 
 song = "";
 play1 = 1;
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 
 
@@ -15,7 +19,31 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized.');
+
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +" leftWristY = " +leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " +rightWristX + " rightWristY = " +rightWristY);
+    }
+
+}
+
+
 
 function draw() {
     image(video, 0, 0, 600, 500);
@@ -23,8 +51,16 @@ function draw() {
 
 function play() {
 
+    
+    
+    song.rate(1);
+
+
 if (play1 == 1) {
+
     song.play();
+    song.setVolume(1);
+    song.rate(1);
     document.getElementById("play_button").innerText = "Pause";
     play1 = 0;
 }    
